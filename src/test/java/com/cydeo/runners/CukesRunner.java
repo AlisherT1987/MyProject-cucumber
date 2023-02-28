16596364dad877d2e48c2f374377780aea3a2888
Alisherb28 <alish.tussupbaev@gmail.com>
2023-02-28 00:02:06 -1000

    My project

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -17,7 +17,6 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@smoke",
        publish = true
 )
 public class CukesRunner {}
